93a7d1f86760a6f8634132d2613e6d9e7182e11a
Tianzong Cheng <oierprime@outlook.com>
2024-07-23 03:50:08 +0800

    feat: draw cup mask

---
+++ seg/draw_mask_cup.py
@@ -0,0 +1,68 @@
+from ultralytics import YOLO
+import cv2
+import torch
+from pathlib import Path
+import numpy as np
+import shutil
+
+# Load a pretrained YOLOv8s-seg Segment model
+model = YOLO("/home/rl/workspace/LLM_Robot/Segmentation/assets/yolov8l-seg.pt")
+
+# Load the input image
+input_image_path = "/home/rl/workspace/LLM_Robot/ZED/output/rgb/000000.png"
+input_image = cv2.imread(input_image_path)
+input_height, input_width = input_image.shape[:2]
+
+# Run inference on the image
+results = model(input_image_path)
+
+output_dir = Path("./output/")
+output_dir.mkdir(parents=True, exist_ok=True)
+
+# Copy the original image to the output folder
+shutil.copy(input_image_path, output_dir / "original_image.jpg")
+
+# Get the class names
+seg_classes = list(results[0].names.values())
+
+# Find the index of the "cup" class
+cup_class_index = seg_classes.index("cup") if "cup" in seg_classes else None
+
+if cup_class_index is None:
+    print("No 'cup' class found in the model's classes.")
+else:
+    for result in results:
+        masks = result.masks.data
+        boxes = result.boxes.data
+        clss = boxes[:, 5]
+
+        # MASK OF ALL INSTANCES OF THE CUP CLASS
+        cup_indices = torch.where(clss == cup_class_index)
+        cup_masks = masks[cup_indices]
+        cup_mask = torch.any(cup_masks, dim=0).int() * 255
+
+        if cup_masks is not None:
+            # Resize the cup mask to the original image size
+            cup_mask_resized = cv2.resize(
+                cup_mask.cpu().numpy(),
+                (input_width, input_height),
+                interpolation=cv2.INTER_NEAREST,
+            )
+
+            # MASK FOR EACH INSTANCE OF THE CUP CLASS
+            for i, cup_index in enumerate(cup_indices[0].cpu().numpy()):
+                single_cup_mask = masks[torch.tensor([cup_index])]
+                single_cup_mask = torch.any(single_cup_mask, dim=0).int() * 255
+                single_cup_mask_resized = cv2.resize(
+                    single_cup_mask.cpu().numpy(),
+                    (input_width, input_height),
+                    interpolation=cv2.INTER_NEAREST,
+                )
+                cv2.imwrite(str(f"./output/cup_{i}.jpg"), single_cup_mask_resized)
+
+                # Create a masked version of the input image for each cup
+                single_cup_mask_resized = single_cup_mask_resized.astype(np.uint8)
+                single_masked_image = cv2.bitwise_and(
+                    input_image, input_image, mask=single_cup_mask_resized
+                )
+                cv2.imwrite(str(f"./output/masked_cup_{i}.jpg"), single_masked_image)
