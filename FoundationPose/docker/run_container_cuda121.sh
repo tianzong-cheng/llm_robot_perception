8e9cc063a39d3c15cb0c02039148c5b09e7eb2fc
Tianzong Cheng <oierprime@outlook.com>
2024-07-23 16:17:56 +0800

    Add FoundationPose docker startup script

---
+++ FoundationPose/docker/run_container_cuda121.sh
@@ -0,0 +1,3 @@
+docker rm -f foundationpose_cuda121
+CATGRASP_DIR=$(pwd)/../
+xhost +  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose_cuda121  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v /home/tianzong:/home/tianzong -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE foundationpose_cuda121:latest bash
